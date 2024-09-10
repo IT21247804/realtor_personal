62af45fdd784619a69bf53244f2908dccd657c40
Chandima <chandimar.online@gmail.com>
2024-09-10 16:30:43 +0530

    feat: update form navigation on submit

--- src/modules/dashboard/properties/components/add-property-form.js
+++ src/modules/dashboard/properties/components/add-property-form.js
@@ -307,7 +307,7 @@ export const AddPropertyForm = () => {
         description: "Property added successfully!",
       });
 
-      // navigate("/dashboard/properties");
+      navigate("/dashboard/properties");
     } catch (error) {
       notification.error({
         message: "Error",
