69712cc531b434ca330abe3d4c3d59a7125e2bb2
Baddewithana P <99246737+IT21247804@users.noreply.github.com>
2025-03-27 22:51:32 +0530

    feat(logout):add logout and animation on scroll.

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { scroller } from 'react-scroll';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import OurServices from './OurServices';
 import ConciergeService from './ConciergeService';
 import ContactUs from '../modules/client/home/components/contact-us';
@@ -10,6 +12,14 @@ const Services = () => {
   const location = useLocation();
 
   useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: false,
+      mirror: true
+    });
+  }, []);
+
+  useEffect(() => {
     // Small delay to ensure the page has loaded
     const timeoutId = setTimeout(() => {
       if (location.hash === '#concierge-service') {
@@ -17,7 +27,7 @@ const Services = () => {
           duration: 800,
           delay: 0,
           smooth: 'easeInOutQuart',
-          offset: -100 // Adjust this value based on your navbar height
+          offset: -100
         });
       }
     }, 100);
@@ -27,16 +37,25 @@ const Services = () => {
 
   return (
     <div>
-      <Element name="our-services">
-        <OurServices />
-      </Element>
+      <div data-aos="fade-up">
+        <Element name="our-services">
+          <OurServices />
+        </Element>
+      </div>
+
       <div className="my-12"></div>
-      <Element name="concierge-service">
-        <ConciergeService />
-      </Element>
-      <Element name="contact-us">
-        <ContactUs />
-      </Element>
+
+     
+        <Element name="concierge-service">
+          <ConciergeService />
+        </Element>
+     
+
+      <div data-aos="fade-up">
+        <Element name="contact-us">
+          <ContactUs />
+        </Element>
+      </div>
     </div>
   );
 };
